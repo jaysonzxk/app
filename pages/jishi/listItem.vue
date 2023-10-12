54d311918476df0200abd7178f06d672013fb887
jaysonzxk <1344173512@qq.com>
2023-10-12 16:39:17 +0700

    ok

---
+++ pages/jishi/listItem.vue
@@ -0,0 +1,19 @@
+<template>
+	<scroll-view scroll-y="true" class="scroll-Y"
+	:style="{backgroundColor: bgc}"
+	refresher-enabled
+	:refresher-triggered="refreshListFlag"
+	refresherrefresh="refreshList"
+	refresherrestore="onRestore"
+	scrolltolower="scrolltolower"
+	>
+		<slot name="list" :list="list"></slot>
+		<u-loadmore style="margin: 20px;"></u-loadmore>
+	</scroll-view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
